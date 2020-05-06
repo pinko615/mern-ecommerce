2814da14d1df3bc0b6714afec09e9d82d480ab39
Pinko <pinko@Pinko-2.local>
2020-05-06 13:03:15 +0200

    add second product

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -8,7 +8,7 @@ function ImageSlider(props) {
             <Carousel autoplay>
                 {props.images.map((image, index) => (
                     <div key={index}>
-                        <img style={{ width:'100%', maxHeight:'150px' }}
+                        <img style={{ width:'100%', height:'100%' }}
                              src={`http://localhost:5000/${image}`} alt="productImage" />
                     </div>
                 ))}
