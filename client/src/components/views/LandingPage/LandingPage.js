63a35ac881791a5040690d613dddba60fdcfd023
Pinko <pinko@Pinko-2.local>
2020-05-06 12:58:33 +0200

    image carousel in prods

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import { Icon, Card, Col, Row } from 'antd'
+import ImageSlider from '../../utils/ImageSlider'
 const { Meta } = Card
 
 
@@ -24,7 +25,7 @@ function LandingPage() {
         return <Col lg={6} md={8} xs={24}>
             <Card
                 hoverable={true}
-                cover
+                cover={<ImageSlider images={product.images} />}
             >
                 <Meta 
                     title={product.title}
